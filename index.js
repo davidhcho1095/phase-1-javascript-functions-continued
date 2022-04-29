69757629fd935bdf633dc60bcbd33befeba56ae4
David Cho <davidhcho1095@gmail.com>
2022-04-28 21:40:02 -0400

    commit tree 50e47c33b8533fe8c632abda41846df4022d5013

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+//Saturday Fun
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+//Monday Work
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+//Wrap Adjective
+function wrapAdjective(adjective = '*'){
+    let inner = function(Default = '||'){
+        return `You are ${adjective}${Default}${adjective}!`;
+    }
+    return inner;
+}
